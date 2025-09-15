f1e201cedd9832abc5694bd42e2491b6fa94e17a
Dley <wesdleyslv4@gmail.com>
2025-09-15 20:07:33 -0300

    C09 - c11

--- E16 - Reforçando Estruturas de Repetição II/javascript.js
+++
@@ -1,7 +0,0 @@
-let somaTotal = 0;
-const qtdeInteracoes = 150;
-for (let i = qtdeInteracoes; i >= 1; i--){
-    somaTotal += i; // equivalente a utilizar somaTotal = somaTotal + i
-}
-console.log('soma total:',somaTotal);
-document.getElementById('resp').innerHTML = `${somaTotal}`
