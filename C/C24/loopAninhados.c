dd23305aa2ae58eb1b7032d55eebd50a1c88d758
Dley <wesdleyslv4@gmail.com>
2025-09-26 12:42:29 -0300

    C24

---
+++ C/C24/loopAninhados.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+    int i;
+    for (i = 1; i <= 10; i++) {
+        int j;
+        for( j = 1; j <=10; j++) {
+            printf("%d x %d = %d\n", i, j, i * j);
+        }
+        printf("\n");
+    }
+}
