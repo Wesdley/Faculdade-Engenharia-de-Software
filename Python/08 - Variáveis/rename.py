5b9e313e6081ea26e549f156c1871d1a8c8ecc63
Dley <wesdleyslv4@gmail.com>
2025-10-19 14:12:49 -0300

    08 - Variáveis

---
+++ Python/08 - Variáveis/rename.py
@@ -0,0 +1,11 @@
+# definir função chamada imprimir_texto
+def imprimir_texto(numero, texto):
+    print("Text é: ",texto)
+    print("Numero é: ",numero)
+
+#colocamos os parametros
+numero_fornecido = 10
+texto_fornecido = "João"
+
+#chamamos a função
+imprimir_texto(numero_fornecido, texto_fornecido)
