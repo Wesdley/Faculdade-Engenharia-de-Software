c706fa4931bfa5b2a5055c2764db17d0f2f4d39a
Dley <wesdleyslv4@gmail.com>
2025-10-19 13:06:14 -0300

    05 - 06 Pratica Python Básica

---
+++ Python/05 - Pratica Python Básico/rename.py
@@ -0,0 +1,18 @@
+# Colocamos atributos
+idade = 25
+nome = "João"
+altura = 1.76
+
+# mostramos esses atributos
+print("Nome:",nome)
+print("Idade:",idade)
+print("Altura:",altura)
+
+# mudamos o valor de idade
+idade = 26
+
+if idade >= 18:
+    print(nome + " é maior de idade")
+else:
+    print(nome + " é menor de idade")
+
---
+++ Python/06 - Pratica Python Básico II/rename.py
@@ -0,0 +1,8 @@
+# média declarada
+a = 20
+b = 5
+# calculamos a média
+media = (a + b) / 2
+# mostramos a média
+print(media)
+
