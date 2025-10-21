1362493ad0de63f8b3a5e995f792c126def50b8e
Dley <wesdleyslv4@gmail.com>
2025-10-21 17:25:20 -0300

    20 - tkinter

--- Python/20 - tkinter/rename.py
+++ Python/20 - tkinter/rename.py
@@ -1,8 +1,18 @@
 from tkinter import * # importa o tkinter
 
+def clickButton ():
+    print("Madara!")
+
 janelaPrincipal = Tk() # cria janela sem nada
 
 texto = Label(master = janelaPrincipal, text = "Minha janela exibida")
-texto.place(x = 50, y = 100)
+texto.pack()
+
+pic = PhotoImage(file="C:/Users/User/Desktop/Curso/Faculdade-Engenharia-de-Software/Python/20 - tkinter/madara.gif")
+logo = Label(master = janelaPrincipal, image = pic)
+logo.pack()
+
+botao = Button(master = janelaPrincipal, text = 'Clique', command = clickButton())
+botao.pack()
 
 janelaPrincipal.mainloop() # mantem ela aberta
