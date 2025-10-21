781de0ce874f0e8f208142c3f8c554eb52ffe6be
Dley <wesdleyslv4@gmail.com>
2025-10-21 14:14:52 -0300

    16 - Parâmetros, procedimentos e funções

--- Python/16 - Parâmetros, procedimentos e funções/rename.py
+++ Python/16 - Parâmetros, procedimentos e funções/rename.py
@@ -46,3 +46,19 @@ print(f"Função principal - x {x}")
 func2(x)
 print(f"Função principal - x {x}")
 
+def taximetro(distancia):
+    def calculaMult():
+        if distancia < 5:
+            return 1.2
+        else:
+            return 1
+
+    multiplicador = calculaMult()
+    largada = 3
+    km_rodado = 2
+    valor = (largada + distancia * km_rodado) * multiplicador
+    return valor
+
+dist = eval(input("Entre com a distancia a ser percorrida em km: \n"))
+pagamento = taximetro(dist)
+print(f'O valor a pagar é R$ {pagamento}')
