0516fd18983955859e56773ba1e79b7a7e032238
Dley <wesdleyslv4@gmail.com>
2025-10-21 13:12:55 -0300

    15 - subprogramas

---
+++ Python/15 - Subprogramas/rename.py
@@ -0,0 +1,12 @@
+def funx ():
+    num = int(input("Digite 1 ou 2: "))
+    if num == 1:
+        soma = num + 1
+        print(soma)
+    elif num == 2:
+        soma = num + 2
+        print(soma)
+    else:
+        print("Opção invalida")
+
+funx() # chamamos a função para funcionar
